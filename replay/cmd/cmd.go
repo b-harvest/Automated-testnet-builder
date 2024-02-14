28301684386b7cbe8e2deb92969300bd8bf15b65
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-15 00:46:45 +0900

    Divide file & functions

--- replay/cmd/cmd.go
+++ replay/cmd/cmd.go
@@ -2,23 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	"log"
-	"time"
-
-	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/spf13/cobra"
-
-	chain "github.com/Canto-Network/Canto/v7/app"
-	"github.com/Canto-Network/Canto/v7/cmd/config"
-	inflationtypes "github.com/Canto-Network/Canto/v7/x/inflation/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/genutil"
-	"github.com/cosmos/cosmos-sdk/x/staking"
-	"github.com/evmos/ethermint/encoding"
-	ethermint "github.com/evmos/ethermint/types"
-	tmlog "github.com/tendermint/tendermint/libs/log"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 var (
@@ -40,30 +24,26 @@ const (
 )
 
 func NewReplayCmd() *cobra.Command {
-	var (
-		newChainId    = "canto_7700-1"
-		initialHeight = int64(1)
-	)
 	cmd := &cobra.Command{
 		Use: "genesis",
 
 		Args: cobra.ExactArgs(2),
 		PreRun: func(_ *cobra.Command, args []string) {
-			sdkConfig := sdk.GetConfig()
-			sdkConfig.SetPurpose(sdk.Purpose)
-			sdkConfig.SetCoinType(ethermint.Bip44CoinType)
-			sdkConfig.SetBech32PrefixForAccount(config.Bech32PrefixAccAddr, config.Bech32PrefixAccPub)
-			sdkConfig.SetBech32PrefixForValidator(config.Bech32PrefixValAddr, config.Bech32PrefixValPub)
-			sdkConfig.SetBech32PrefixForConsensusNode(config.Bech32PrefixConsAddr, config.Bech32PrefixConsPub)
-			sdkConfig.SetAddressVerifier(AddressVerifier)
-			sdkConfig.SetFullFundraiserPath(ethermint.BIP44HDPath)
-			if err := sdk.RegisterDenom(DisplayDenom, sdk.OneDec()); err != nil {
-				panic(err)
-			}
-
-			if err := sdk.RegisterDenom(BaseDenom, sdk.NewDecWithPrec(1, ethermint.BaseDenomUnit)); err != nil {
-				panic(err)
-			}
+			//sdkConfig := sdk.GetConfig()
+			//sdkConfig.SetPurpose(sdk.Purpose)
+			//sdkConfig.SetCoinType(ethermint.Bip44CoinType)
+			//sdkConfig.SetBech32PrefixForAccount(config.Bech32PrefixAccAddr, config.Bech32PrefixAccPub)
+			//sdkConfig.SetBech32PrefixForValidator(config.Bech32PrefixValAddr, config.Bech32PrefixValPub)
+			//sdkConfig.SetBech32PrefixForConsensusNode(config.Bech32PrefixConsAddr, config.Bech32PrefixConsPub)
+			//sdkConfig.SetAddressVerifier(AddressVerifier)
+			//sdkConfig.SetFullFundraiserPath(ethermint.BIP44HDPath)
+			//if err := sdk.RegisterDenom(DisplayDenom, sdk.OneDec()); err != nil {
+			//	panic(err)
+			//}
+			//
+			//if err := sdk.RegisterDenom(BaseDenom, sdk.NewDecWithPrec(1, ethermint.BaseDenomUnit)); err != nil {
+			//	panic(err)
+			//}
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) < 2 {
@@ -72,117 +52,8 @@ func NewReplayCmd() *cobra.Command {
 
 			dir := args[0]
 			validatorFile := args[1]
-
 			cmd.SilenceUsage = true
-
-			db, err := sdk.NewLevelDB("application", dir)
-			if err != nil {
-				panic(err)
-			}
-			defer db.Close()
-
-			// Load previous height
-			app := chain.NewCanto(tmlog.NewNopLogger(), db, nil, false, map[int64]bool{}, "localnet", 0, false, encoding.MakeConfig(chain.ModuleBasics), simapp.EmptyAppOptions{})
-			height := app.LastBlockHeight()
-			fmt.Printf("LastBlockHeight: %d\n", height)
-
-			if err := app.LoadHeight(height - 1); err != nil {
-				panic(fmt.Errorf("failed to load height: %w", err))
-			}
-
-			ctx := app.BaseApp.NewContext(true, tmproto.Header{})
-			ctx = ctx.WithBlockHeight(height)
-
-			// Set governance params
-			votingParams := app.GovKeeper.GetVotingParams(ctx)
-			votingParams.VotingPeriod = 30 * time.Second
-			tallyParams := app.GovKeeper.GetTallyParams(ctx)
-			tallyParams.Quorum = sdk.MustNewDecFromStr("0.000001")
-			app.GovKeeper.SetVotingParams(ctx, votingParams)
-			app.GovKeeper.SetTallyParams(ctx, tallyParams)
-
-			// Get staking bond denom
-			bondDenom := app.StakingKeeper.BondDenom(ctx)
-
-			// Read Validators from file
-			validatorList, err := ReadValidatorInfosFile(validatorFile, bondDenom)
-			if err != nil {
-				panic(fmt.Errorf("Failed to read validator file: %s\n%s", validatorFile, err.Error()))
-			}
-
-			for _, v := range validatorList {
-
-				if err := app.InflationKeeper.MintCoins(ctx, v.VotingPower[0]); err != nil {
-					return err
-				}
-				if err := app.BankKeeper.SendCoinsFromModuleToAccount(ctx, inflationtypes.ModuleName, v.GetAddress(), v.VotingPower); err != nil {
-					return err
-				}
-
-				if err := v.CreateValidator(ctx, &app.StakingKeeper, app.AppCodec()); err != nil {
-					return err
-				}
-			}
-
-			//bal_amoint, _ := sdk.NewIntFromString("1000000000.000000000000000000")
-			//stake_amount, _ := sdk.NewIntFromString("500000000.000000000000000000")
-			//val_coin := sdk.NewCoin(bondDenom, bal_amoint)
-			//
-			//val := NewValidator(
-			//	"canto1cr6tg4cjvux00pj6zjqkh6d0jzg7mksapardz2",
-			//	sdk.NewCoins(val_coin),
-			//	sdk.NewCoin(bondDenom, stake_amount),
-			//	"{\"@type\": \"/cosmos.crypto.ed25519.PubKey\",\"key\":\"CzUC2BDiSxOBJ4tKxd9flLfZy6nrSKJ8YE7mfiHnhv8=\"}",
-			//	"val1",
-			//)
-
-			//bal_amoint2, _ := sdk.NewIntFromString("10.000000000000000000")
-			//stake_amount2, _ := sdk.NewIntFromString("10.000000000000000000")
-			//val2_coin := sdk.NewCoin(bondDenom, bal_amoint2)
-			//val2 := NewValidator(
-			//	"canto1ywps7lrfjm8cww04pt9xad494u8qwhvdsjzzan",
-			//	sdk.NewCoins(val2_coin),
-			//	sdk.NewCoin(bondDenom, stake_amount2),
-			//	"{\"@type\": \"/cosmos.crypto.ed25519.PubKey\",\"key\":\"GmAFwR4Z6iFTv6yzMETDigK38Nh38TDimLGvCaKkzvo=\"}",
-			//	"val2",
-			//)
-
-			staking.EndBlocker(ctx, app.StakingKeeper)
-			staking.BeginBlocker(ctx, app.StakingKeeper)
-
-			log.Println("Exporting app state and validators...")
-
-			exported, err := app.ExportAppStateAndValidators(false, nil)
-			if err != nil {
-				return fmt.Errorf("failed to export app state and validators: %w", err)
-			}
-
-			genDoc := &tmtypes.GenesisDoc{
-				GenesisTime:   time.Now(),
-				ChainID:       newChainId,
-				AppState:      exported.AppState,
-				Validators:    exported.Validators,
-				InitialHeight: initialHeight,
-				ConsensusParams: &tmproto.ConsensusParams{
-					Block: tmproto.BlockParams{
-						MaxBytes:   exported.ConsensusParams.Block.MaxBytes,
-						MaxGas:     exported.ConsensusParams.Block.MaxGas,
-						TimeIotaMs: 1000,
-					},
-					Evidence: tmproto.EvidenceParams{
-						MaxAgeNumBlocks: exported.ConsensusParams.Evidence.MaxAgeNumBlocks,
-						MaxAgeDuration:  exported.ConsensusParams.Evidence.MaxAgeDuration,
-						MaxBytes:        exported.ConsensusParams.Evidence.MaxBytes,
-					},
-					Validator: tmproto.ValidatorParams{
-						PubKeyTypes: exported.ConsensusParams.Validator.PubKeyTypes,
-					},
-				},
-			}
-
-			log.Println("Exporting genesis file...")
-
-			return genutil.ExportGenesisFile(genDoc, "exported-genesis.json")
+			return ReplayGenesis(dir, validatorFile)
 		},
 	}
 	return cmd
--- replay/cmd/init.go
+++ replay/cmd/init.go
@@ -24,127 +24,136 @@ func ChainInitCmd() *cobra.Command {
 			balAmount := args[1]
 			stakeAmount := args[2]
 
-			var (
-				binary           = "cantod"
-				mnemonicList     []string
-				rawValidatorList RawValidatorList
-
-				exportFilePath = "vali-info.yaml"
-			)
-
-			for i := 0; i < count; i++ {
-				moniker := "validator-" + strconv.Itoa(i)
-
-				var initBuffer bytes.Buffer
-				initCmd := exec.Command(binary, "--home", moniker, "init", "--chain-id", "canto_7700-1", moniker)
-				initCmd.Stdout = &initBuffer
-				initCmd.Stderr = &initBuffer
-				if err = initCmd.Run(); err != nil {
-					panic(fmt.Errorf("%s\n", initBuffer.String()))
-				}
-
-				generateMnemonicBuffer := new(bytes.Buffer)
-				// Generate mnemonic
-				generateMnemonicCmd := exec.Command(binary, "keys", "mnemonic")
-				generateMnemonicCmd.Stdout = generateMnemonicBuffer
-				generateMnemonicCmd.Stderr = generateMnemonicBuffer
-
-				if err = generateMnemonicCmd.Run(); err != nil {
-					panic(fmt.Errorf("%s\n", generateMnemonicBuffer.String()))
-				}
-
-				mnemonic := generateMnemonicBuffer.String()
-				mnemonicList = append(mnemonicList, mnemonic)
-
-				var mnemonicBuffer bytes.Buffer
-				_, err = mnemonicBuffer.Write([]byte(mnemonic))
-				if err != nil {
-					panic(err)
-				}
-
-				//keysAddBuffer, keysAddCmdW, err := os.Pipe()
-				var keysAddBuffer bytes.Buffer
-				if err != nil {
-					panic(err)
-				}
-				keysAddCmd := exec.Command(binary, "--home", moniker,
-					"keys", "add", moniker, "--recover", "--keyring-backend", "test", "--output", "json")
-				keysAddCmd.Stdin = &mnemonicBuffer
-				keysAddCmd.Stdout = &keysAddBuffer
-				keysAddCmd.Stderr = &keysAddBuffer
-
-				if err = keysAddCmd.Run(); err != nil {
-					panic(fmt.Errorf("%s\n", keysAddBuffer.String()))
-				}
-
-				var jqAddressBuffer bytes.Buffer
-				jqAddress := exec.Command("jq", "-r", ".address")
-				jqAddress.Stdin = &keysAddBuffer
-				jqAddress.Stdout = &jqAddressBuffer
-				jqAddress.Stderr = &jqAddressBuffer
-
-				if err = jqAddress.Run(); err != nil {
-					panic(fmt.Errorf("%s\n", jqAddressBuffer.String()))
-				}
-
-				address := jqAddressBuffer.String()
-
-				var validatorKeyBuffer bytes.Buffer
-				validatorKeyCmd := exec.Command("cantod", "tendermint", "show-validator", "--home", moniker)
-				validatorKeyCmd.Stdout = &validatorKeyBuffer
-				validatorKeyCmd.Stderr = &validatorKeyBuffer
-
-				if err = validatorKeyCmd.Run(); err != nil {
-					panic(fmt.Errorf("%s\n", validatorKeyBuffer.String()))
-				}
-
-				validatorKey := validatorKeyBuffer.String()
-
-				//// Read priv_validator_key.json
-				//privValidatorKeyBytes, err := os.ReadFile(fmt.Sprintf("%s/config/priv_validator_key.json", moniker))
-				//if err != nil {
-				//	return err
-				//}
-				//
-				//var privValidatorKey PrivValidatorKey
-				//if err = json.Unmarshal(privValidatorKeyBytes, &privValidatorKey); err != nil {
-				//	return err
-				//}
-
-				// Cut prefix '\n'
-				address, _ = strings.CutPrefix(address, "\n")
-				address, _ = strings.CutSuffix(address, "\n")
-				mnemonic, _ = strings.CutPrefix(mnemonic, "\n")
-				mnemonic, _ = strings.CutSuffix(mnemonic, "\n")
-				validatorKey, _ = strings.CutPrefix(validatorKey, "\n")
-				validatorKey, _ = strings.CutSuffix(validatorKey, "\n")
-
-				fmt.Printf("Moniker: %s \nmnemonic: %s \naddress: %s\n", moniker, mnemonic, address)
-
-				// Generate RawValidator
-				rawValidatorList = append(rawValidatorList, RawValidator{
-					Moniker:      moniker,
-					Address:      address,
-					BalAmount:    balAmount,
-					StakeAmount:  stakeAmount,
-					ValidatorKey: validatorKey,
-					Mnemonic:     mnemonic,
-				})
-			}
-
-			marshal, err := yaml.Marshal(rawValidatorList)
+			err = ChainInit(count, balAmount, stakeAmount)
 			if err != nil {
-				panic(err)
+				return err
 			}
-
-			err = os.WriteFile(exportFilePath, marshal, 755)
-			if err != nil {
-				panic(err)
-			}
-
 			return nil
 		},
 	}
 
 	return &cmd
+}
+
+func ChainInit(count int, balAmount, stakeAmount string) error {
+	var (
+		err              error
+		binary           = "cantod"
+		mnemonicList     []string
+		rawValidatorList RawValidatorList
+
+		exportFilePath = "vali-info.yaml"
+	)
+
+	for i := 0; i < count; i++ {
+		moniker := "validator-" + strconv.Itoa(i)
+
+		var initBuffer bytes.Buffer
+		initCmd := exec.Command(binary, "--home", moniker, "init", "--chain-id", "canto_7700-1", moniker)
+		initCmd.Stdout = &initBuffer
+		initCmd.Stderr = &initBuffer
+		if err = initCmd.Run(); err != nil {
+			panic(fmt.Errorf("%s\n", initBuffer.String()))
+		}
+
+		generateMnemonicBuffer := new(bytes.Buffer)
+		// Generate mnemonic
+		generateMnemonicCmd := exec.Command(binary, "keys", "mnemonic")
+		generateMnemonicCmd.Stdout = generateMnemonicBuffer
+		generateMnemonicCmd.Stderr = generateMnemonicBuffer
+
+		if err = generateMnemonicCmd.Run(); err != nil {
+			panic(fmt.Errorf("%s\n", generateMnemonicBuffer.String()))
+		}
+
+		mnemonic := generateMnemonicBuffer.String()
+		mnemonicList = append(mnemonicList, mnemonic)
+
+		var mnemonicBuffer bytes.Buffer
+		_, err = mnemonicBuffer.Write([]byte(mnemonic))
+		if err != nil {
+			panic(err)
+		}
+
+		//keysAddBuffer, keysAddCmdW, err := os.Pipe()
+		var keysAddBuffer bytes.Buffer
+		if err != nil {
+			panic(err)
+		}
+		keysAddCmd := exec.Command(binary, "--home", moniker,
+			"keys", "add", moniker, "--recover", "--keyring-backend", "test", "--output", "json")
+		keysAddCmd.Stdin = &mnemonicBuffer
+		keysAddCmd.Stdout = &keysAddBuffer
+		keysAddCmd.Stderr = &keysAddBuffer
+
+		if err = keysAddCmd.Run(); err != nil {
+			panic(fmt.Errorf("%s\n", keysAddBuffer.String()))
+		}
+
+		var jqAddressBuffer bytes.Buffer
+		jqAddress := exec.Command("jq", "-r", ".address")
+		jqAddress.Stdin = &keysAddBuffer
+		jqAddress.Stdout = &jqAddressBuffer
+		jqAddress.Stderr = &jqAddressBuffer
+
+		if err = jqAddress.Run(); err != nil {
+			panic(fmt.Errorf("%s\n", jqAddressBuffer.String()))
+		}
+
+		address := jqAddressBuffer.String()
+
+		var validatorKeyBuffer bytes.Buffer
+		validatorKeyCmd := exec.Command("cantod", "tendermint", "show-validator", "--home", moniker)
+		validatorKeyCmd.Stdout = &validatorKeyBuffer
+		validatorKeyCmd.Stderr = &validatorKeyBuffer
+
+		if err = validatorKeyCmd.Run(); err != nil {
+			panic(fmt.Errorf("%s\n", validatorKeyBuffer.String()))
+		}
+
+		validatorKey := validatorKeyBuffer.String()
+
+		//// Read priv_validator_key.json
+		//privValidatorKeyBytes, err := os.ReadFile(fmt.Sprintf("%s/config/priv_validator_key.json", moniker))
+		//if err != nil {
+		//	return err
+		//}
+		//
+		//var privValidatorKey PrivValidatorKey
+		//if err = json.Unmarshal(privValidatorKeyBytes, &privValidatorKey); err != nil {
+		//	return err
+		//}
+
+		// Cut prefix '\n'
+		address, _ = strings.CutPrefix(address, "\n")
+		address, _ = strings.CutSuffix(address, "\n")
+		mnemonic, _ = strings.CutPrefix(mnemonic, "\n")
+		mnemonic, _ = strings.CutSuffix(mnemonic, "\n")
+		validatorKey, _ = strings.CutPrefix(validatorKey, "\n")
+		validatorKey, _ = strings.CutSuffix(validatorKey, "\n")
+
+		fmt.Printf("Moniker: %s \nmnemonic: %s \naddress: %s\n", moniker, mnemonic, address)
+
+		// Generate RawValidator
+		rawValidatorList = append(rawValidatorList, RawValidator{
+			Moniker:      moniker,
+			Address:      address,
+			BalAmount:    balAmount,
+			StakeAmount:  stakeAmount,
+			ValidatorKey: validatorKey,
+			Mnemonic:     mnemonic,
+		})
+	}
+
+	marshal, err := yaml.Marshal(rawValidatorList)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(exportFilePath, marshal, 755)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
 }
